3911e38540b17c1791dbc99a9c60df105c41c612
Leo Basco <leoanjelobasco@gmail.com>
2017-12-07 12:55:37 +0800

    Initial commit

---
+++ app/src/main/java/com/leobasco/labquiz_4itb_12072017/MainActivity.java
@@ -0,0 +1,59 @@
+package com.leobasco.labquiz_4itb_12072017;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    EditText et_username;
+    EditText et_password;
+    Button rem;
+    Button login;
+    FileOutputStream fos;
+    FileInputStream fis;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et_username = (EditText) findViewById(R.id.et_username);
+        et_password = (EditText) findViewById(R.id.et_password);
+        rem = (Button) findViewById(R.id.rem);
+        login = (Button) findViewById(R.id.login);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+    }
+    public void RememberMe (View view) {
+        String message = rem.getText().toString();
+        try {
+            fos = openFileOutput("output.txt", Context.MODE_PRIVATE);
+            fos.write(message.getBytes());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        Toast.makeText(this, "Message saved!", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
